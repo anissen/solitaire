b33a69cdf8a1337c2a122b317526de4e0e2ee3a9
Anders Nissen <andnis@gmail.com>
2017-02-25 21:08:12 +0100

    Push android build to itch

--- push_to_itch.sh
+++ push_to_itch.sh
@@ -1,5 +1,11 @@
+# build for web
 flow build web
 butler push bin/web anissen/solitaire:web
 
+# build for mac
 flow build mac
 butler push bin/mac64 anissen/solitaire:mac
+
+# build for android
+cd android.project && ./gradlew assembleRelease && cd ..
+butler push android.project/app/build/outputs/apk/app-release-unsigned.apk anissen/solitaire:android
