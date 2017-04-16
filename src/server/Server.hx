28c8e232f55728558aca523bf4a606907c064984
Anders Nissen <andnis@gmail.com>
2017-04-17 00:17:33 +0200

    Persist db

--- src/server/Server.hx
+++ src/server/Server.hx
@@ -5,8 +5,25 @@ import js.node.http.ServerResponse;
 import js.node.http.IncomingMessage;
 import js.node.Url;
 
+using Lambda;
+
+typedef Highscore = { name :String, score :Int /*, seed :String, time :Date */ };
+
 class Server {
     static public function main() {
+        var highscore_file = 'highscores.json';
+        var highscores :Array<Highscore> = (sys.FileSystem.exists(highscore_file) ? haxe.Json.parse(sys.io.File.getContent(highscore_file)) : []);
+        
+        function get_highscores() {
+            return highscores;
+        }
+
+        function set_highscore(highscore :Highscore) {
+            var highscores = get_highscores();
+            highscores.push(highscore);
+            sys.io.File.saveContent(highscore_file, haxe.Json.stringify(highscores));
+        }
+
         var server = Http.createServer(function(request :IncomingMessage, response :ServerResponse) {
             function send(data :Dynamic, status :Int) {
                 response.setHeader("Content-Type","text/json");
@@ -18,21 +35,15 @@ class Server {
 
             var params = Url.parse(request.url, true);
             var query :haxe.DynamicAccess<String> = params.query;
-            // var query :Dynamic = params.query;
             
-            var queryString = haxe.Json.stringify(query);
             switch (params.pathname) {
-                case '/get_highscore': ok([{ score: 100, name: 'Aaa' }, { score: 90, name: 'Bbb' }]);
-                case '/set_highscore' if (query.exists('score') && query.exists('name')): trace('setting highscore: ${query.get('score')} for ${query.get('name')}'); ok({ status: 'success' });
-                // case '/set_highscore': trace('setting highscore: ${query.scores} for ${query.name}'); ok({ status: 'success' });
+                case '/get_highscore': ok(get_highscores());
+                case '/set_highscore' if (query.exists('score') && query.exists('name')):
+                    trace('setting highscore: ${query.get('score')} for ${query.get('name')}');
+                    set_highscore({ name: query.get('name'), score: Std.parseInt(query.get('score')) /*, seed: query.get('seed'), time:  Date.fromString(query.get('time')) */ });
+                    ok(get_highscores().filter(function(h) { return h.score > 50; }));
                 case _: error({ error: 'Unknown endpoint "${params.pathname}"'});
             }
-            // trace('query: $queryString');
-            // trace('pathname: ${params.pathname}');
-
-            // response.setHeader("Content-Type","text/json");
-            // response.writeHead(200);
-            // response.end(haxe.Json.stringify(data));
             trace('You got served!');
         });
         server.listen(1337, "localhost");
