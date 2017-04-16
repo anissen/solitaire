188b1675f17d50a376e32f1c064b5e215cff3508
Anders Nissen <andnis@gmail.com>
2017-04-17 00:18:39 +0200

    Clean-up

--- src/server/Client.hx
+++ src/server/Client.hx
@@ -2,10 +2,9 @@ package server;
 
 class Client {
     static function get_highscores() {
-        // return haxe.Http.requestUrl("http://localhost:1337");
         var http = new haxe.Http("http://localhost:1337/set_highscore");
         http.addParameter('name', 'Anders');
-        http.addParameter('score', '42');
+        http.addParameter('score', '37');
         http.addParameter('blah', '66');
         http.onError = function(data) {
             trace('error: $data');
@@ -23,30 +22,4 @@ class Client {
         get_highscores();
         trace('done');    
     }
-
-    // static function main() {
-    // var http = new haxe.Http("http://localhost:5000");
-    // http.onError = function(data) {
-	// 	trace('error: $data');
-    // }
-    // http.onStatus = function(data) {
-	// 	trace('status: $data');
-    // }
-    // http.onData = function(data) {
-	// 	trace('data: $data');
-    // }
-    // http.request();
-    // trace('done');
-
-        // var s = new sys.net.Socket();
-        // s.connect(new sys.net.Host("localhost"), 5000);
-        // while( true ) {
-        //     var l = s.input.readLine();
-        //     trace(l);
-        //     if( l == "exit" ) {
-        //         s.close();
-        //         break;
-        //     }
-        // }
-    // }
 }
