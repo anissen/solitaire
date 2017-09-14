225b180cb705c82e3ca9753368a3134f432ff534
Anders Nissen <andnis@gmail.com>
2017-09-14 20:05:16 +0200

    Inertia dragging on game over screen

--- src/game/components/DragPan.hx
+++ src/game/components/DragPan.hx
@@ -11,14 +11,21 @@ class DragPan extends Component {
     public var y_bottom :Float;
     public var button : MouseButton;
 
-    var dragging : Bool = false;
-    var drag_start : Vector;
-    var drag_start_pos : Vector;
-    var visual : luxe.Visual;
+    var dragging :Bool = false;
+    var drag_start :Vector;
+    var drag_start_pos :Vector;
+    var visual :luxe.Visual;
+
+    var drag_velocity :Float;
+    var inertia_duration :Float = 0.5;
+    var inertia_time :Float;
 
     override function init() {
         drag_start = new Vector();
         drag_start_pos = new Vector();
+        
+        drag_velocity = 0;
+        inertia_time = 0;
 
         button = MouseButton.left;
 
@@ -29,6 +36,7 @@ class DragPan extends Component {
     override function onmousedown(e :MouseEvent) {
         if (!dragging && e.button == button) {
             dragging = true;
+            inertia_time = 0;
             drag_start.set_xy(e.pos.x, e.pos.y);
             drag_start_pos.set_xy(pos.x, pos.y);
         }
@@ -37,14 +45,25 @@ class DragPan extends Component {
     override function onmouseup(e :MouseEvent) {
         if (e.button == button && dragging) {
             dragging = false;
+            inertia_time = inertia_duration;
         }
     }
 
     override function onmousemove(e :MouseEvent) {
         if (dragging) {
-            var diffy = (drag_start.y - e.pos.y); // / camera.zoom;
-            pos.y = luxe.utils.Maths.clamp(drag_start_pos.y - diffy, y_top, y_bottom);
-            // pos.y = drag_start_pos.y - diffy;
+            var drag_diff_y = (drag_start.y - e.pos.y);
+            var previous_pos = pos.y;
+            pos.y = luxe.utils.Maths.clamp(drag_start_pos.y - drag_diff_y, y_top, y_bottom);
+            drag_velocity = pos.y - previous_pos;
+        }
+    }
+
+    override function update(dt :Float) {
+        if (inertia_time > 0) {
+            pos.y += drag_velocity;
+            var progress = (inertia_duration - inertia_time) / inertia_duration;
+            drag_velocity = luxe.utils.Maths.lerp(drag_velocity, 0, progress);
+            inertia_time -= dt;
         }
     }
 }
