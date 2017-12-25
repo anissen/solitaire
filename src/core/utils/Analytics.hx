578f202dc0a905dffc796ade8f0b1d8dbdb0fe9f
Anders Nissen <andnis@gmail.com>
2017-12-25 19:23:20 +0100

    Use akifox async http

--- src/core/utils/Analytics.hx
+++ src/core/utils/Analytics.hx
@@ -5,60 +5,42 @@ class Analytics {
     public static var client_id :String;
 
     public static function event(category :String, action :String, ?label :String, ?value :Int) {
-        var rand :Int = Std.int(Math.random() * 100000000);
         var maybe_label = (label != null ? '&el=$label' : '');
         var maybe_value = (value != null ? '&ev=$value' : '');
-        var url = 'https://www.google-analytics.com/collect?v=1&tid=$tracking_id&cid=$client_id&t=event&ec=$category&ea=$action$maybe_label$maybe_value&z=$rand';
-        var http = new haxe.Http(url);
-        // http.onData = function(data) {
-        //     trace('Result: $data');
-        // };
-        // http.onError = function(err) {
-        //     trace('Error: $err');
-        // };
-        // http.onStatus = function(status) {
-        //     trace('Status: $status');
-        // };
-        #if web
-        http.async = true;
-        #end
-        http.request();
+        analytics_request('t=event&ec=$category&ea=$action$maybe_label$maybe_value');
     }
 
-    // public static function screen(screen :String) {
-    //     var rand :Int = Std.int(Math.random() * 100000000);
-    //     var url = 'https://www.google-analytics.com/collect?v=1&tid=$tracking_id&cid=$client_id&t=screenview&an=solitaire&av=0.4.0&aid=anissen.solitaire&cd=$screen&z=$rand';
-    //     var http = new haxe.Http(url);
-    //     // http.onData = function(data) {
-    //     //     trace('Result: $data');
-    //     // };
-    //     // http.onError = function(err) {
-    //     //     trace('Error: $err');
-    //     // };
-    //     // http.onStatus = function(status) {
-    //     //     trace('Status: $status');
-    //     // };
-    //     #if web
-    //     http.async = true;
-    //     #end
-    //     http.request();
-    // }
     public static function screen(screen :String) {
+        analytics_request('t=pageview&dh=solitaire&dp=$screen&dt=$screen');
+    }
+
+    static function analytics_request(url_part :String) {
         var rand :Int = Std.int(Math.random() * 100000000);
-        var url = 'https://www.google-analytics.com/collect?v=1&tid=$tracking_id&cid=$client_id&t=pageview&dh=solitaire&dp=$screen&dt=$screen&z=$rand';
-        var http = new haxe.Http(url);
-        // http.onData = function(data) {
-        //     trace('Result: $data');
-        // };
-        // http.onError = function(err) {
-        //     trace('Error: $err');
-        // };
-        // http.onStatus = function(status) {
-        //     trace('Status: $status');
-        // };
-        #if web
-        http.async = true;
-        #end
-        http.request();
+        var url = 'https://www.google-analytics.com/collect?v=1&tid=$tracking_id&cid=$client_id&$url_part&z=$rand';
+        var request = new com.akifox.asynchttp.HttpRequest({ url: url });
+        // callback : function(response:HttpResponse):Void {
+        //     if (response.isOK) {
+        //         trace(response.content);
+        //         trace('DONE ${response.status}');
+        //     } else {
+        //         trace('ERROR ${response.status} ${response.error}');
+        //     }
+        // }  
+        request.send();
+
+        // #if web
+        // var http = new haxe.Http(url);
+        // // http.onData = function(data) {
+        // //     trace('Result: $data');
+        // // };
+        // // http.onError = function(err) {
+        // //     trace('Error: $err');
+        // // };
+        // // http.onStatus = function(status) {
+        // //     trace('Status: $status');
+        // // };
+        // http.async = true;
+        // http.request();
+        // #end
     }
 }
