0cd3d10292d6fb2fb32b5a0ca694ca08a0fe8f75
Anders Nissen <andnis@gmail.com>
2018-01-03 20:57:14 +0100

    Missing

--- src/game/components/DragOver.hx
+++ src/game/components/DragOver.hx
@@ -7,10 +7,14 @@ import luxe.Input.MouseEvent;
 class DragOver extends Component {
     var sprite :Sprite;
     var callback :Sprite->Void;
+    var left_callback :Sprite->Void;
+    var was_inside :Bool;
 
-    public function new(callback :Sprite->Void) {
+    public function new(callback :Sprite->Void, ?left_callback :Sprite->Void) {
         super({ name: 'DragOver' });
         this.callback = callback;
+        this.left_callback = left_callback;
+        was_inside = false;
     }
 
     override function onadded() {
@@ -32,7 +36,11 @@ class DragOver extends Component {
         var world_pos = Luxe.camera.screen_point_to_world(event.pos);
         if (sprite.point_inside(world_pos)) {
         //     Luxe.events.fire('dragging', sprite);
+            was_inside = true;
             callback(sprite);
+        } else if (was_inside && left_callback != null) {
+            was_inside = false;
+            left_callback(sprite);
         }
     }
 }
